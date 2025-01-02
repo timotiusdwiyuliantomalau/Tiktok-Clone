af823b2f5b805689214f4413e5bc9c01cd713ca0
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2025-01-02 19:17:59 +0700

    Update

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default defineNuxtPlugin((NuxtApp) => {
   axios.defaults.withCredentials = true;
-  axios.defaults.baseURL = "https://tiktok-clone-api-sigma.vercel.app";
+  axios.defaults.baseURL = "https://tiktok-clone-api-production.up.railway.app";
 
   return {
     provide: {
